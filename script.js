cac1700a3dd4afabd6dc941befd9fedf877860d2
BombardiloRuslanillo <ruslanjolmuradov@gmail.com>
2025-09-14 10:02:56 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+
+
+let ism = prompt( 'isming nima?');
+let familiya = prompt( 'familiyangizni yozing');
+let yosh = prompt( 'yoshing nechida?');
+let manzil = prompt( 'qayerda yashaysan?');
+let fan = prompt( 'qanaqa yoqtirgan faning?');
+alert(`malumot uchin raxmat!  ${ism}!`);
+
+console.log(ism);
+console.log(familiya);
+console.log(yosh);
+console.log(manzil);
+console.log(fan);
+
+
+
+
+
+
+
+
+
+
+
+
+const pi = 3.14;
